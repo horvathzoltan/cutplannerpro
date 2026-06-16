43651401a32c9802dcf82c780b95972481986dd5
Horváth Zoltán <zoli@ZoliThinkPadT60.localdomain>
2026-06-16 15:50:20 +0200

    maki1123

--- service/cutting/instruction/cuttinginstructionutils.h
+++ service/cutting/instruction/cuttinginstructionutils.h
@@ -477,7 +477,7 @@ inline QString priorityIconFor(const QDate& dueDate)
 static const QStringList GROUP_ICONS = {
     "🍎", // A - Alma
     "🐸", // B - Béka
-    "🐈‍⬛", // C - Macska
+    "🐈‍", // C - Macska
     "🦇", // D - Denevér
     "🐭", // E - Egér
     "🍦", // F - Fagyi
